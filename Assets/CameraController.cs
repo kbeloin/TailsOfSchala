d65f8e1d958661ac1dea613c6407860d5f3e1999
Mandayen <66381432+Mandayen@users.noreply.github.com>
2020-07-15 22:28:32 -0500

    I've created a smooth camera follow

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,19 +18,30 @@ public class CameraController : MonoBehaviour
 
     public Transform player;
 
+    Vector2 distance;
+
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+        targetPosition = player.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         viewPortSize = (cam.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)) - cam.ScreenToWorldPoint(Vector2.zero)) * viewPortFactor;
 
-        targetPosition = player.position;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, followDuration, maximumFollowSpeed);
+        distance = player.position - transform.position;
+        if (Mathf.Abs(distance.x) > viewPortSize.x / 2)
+        {
+            targetPosition.x = player.position.x - (viewPortSize.x / 2 * Mathf.Sign(distance.x));
+        }
+        if (Mathf.Abs(distance.y) > viewPortSize.y / 2)
+        {
+            targetPosition.y = player.position.y - (viewPortSize.y / 2 * Mathf.Sign(distance.y));
+        }
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition - new Vector3(0, 0, 10), ref currentVelocity, followDuration, maximumFollowSpeed);
     }
 
     void OnDrawGizmos()
