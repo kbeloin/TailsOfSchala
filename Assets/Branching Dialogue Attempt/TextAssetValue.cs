9b3b7d66a9057014304f5394bd5c451a1506f72d
Mandayen <66381432+Mandayen@users.noreply.github.com>
2020-07-19 17:07:11 -0500

    Dialogue attempt UI + ink + textMeshPro elements

---
+++ Assets/Branching Dialogue Attempt/TextAssetValue.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/New Dialogue Value", fileName = "New Dialogue Asset")]
+public class TextAssetValue : ScriptableObject
+{
+    public TextAsset value;
+}
+
