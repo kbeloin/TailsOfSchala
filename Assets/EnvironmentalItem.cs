6eb77fdf3e9fb49b766b3b3962b2652e948a217a
Adam Emerson <adamgemerson@gmail.com>
2020-07-24 16:41:21 -0500

    Bedroom curtains now work with new iteminteraction script

---
+++ Assets/EnvironmentalItem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentalItem : MonoBehaviour {
+    public bool isInteractable;
+    public bool interactionSwitch;
+
+    public bool playerInRange;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            PlayerInteract(); // Player interacts with item.  Activates animator to change states.
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    void PlayerInteract()
+    {
+        interactionSwitch = !interactionSwitch;
+        animator.SetBool("interactionSwitch", interactionSwitch);
+
+        //Code for displaying dialogue goes here.
+
+    }
+}
+
