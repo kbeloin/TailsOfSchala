6afca8a86b851d6bf29364fc0c08ce412a919171
Melanie Cooper <melanie@cooperbold.com>
2020-08-16 22:12:38 -0700

    Finish rough sorting of files

--- Assets/[Branching Dialogue Attempt]/TextAssetValue.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(menuName = "Scriptable Objects/New Dialogue Value", fileName = "New Dialogue Asset")]
-public class TextAssetValue : ScriptableObject
-{
-    public TextAsset value;
-}
-
