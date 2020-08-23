a94113e16f64c410424ceffbbc424660c4662ca1
melanie <melaniecooper@Melanies-iMac-2.local>
2020-08-22 17:54:41 -0700

    Some final cleanup for final push

---
+++ Assets/Code/Scripts/Tools/EnvironmentalItem.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using UnityEngine;
+
+public class EnvironmentalItem : MonoBehaviour {
+    public bool isInteractable;
+    public bool interactionSwitch;
+
+    public bool playerInRange;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            PlayerInteract(); // Player interacts with item.  Activates animator to change states.
+        }
+
+				if (Input.GetKeyDown(KeyCode.Q) && playerInRange)
+        {
+          Debug.Log("Attacked this thing!");
+					PlayerInteract();
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = true;
+            //spriteRenderer.material = highlightMaterial;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = false;
+            //spriteRenderer.material = matDefault;
+        }
+    }
+
+    void PlayerInteract()
+    {
+        interactionSwitch = !interactionSwitch;
+        animator.SetBool("interactionSwitch", interactionSwitch);
+
+        //Code for displaying dialogue goes here.
+
+    }
+}
+
