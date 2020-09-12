ee8e93f6221a952ab4cee381603aec91c5c9a006
melanie <melanie@cooperbold.com>
2020-09-12 15:02:18 -0700

    Add a tooltip to tell players how to interact with items, and dismiss it on interactions

---
+++ Assets/Code/Scripts/UI/DismissTooltip.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DismissTooltip : MonoBehaviour
+{
+    public GameObject tooltip;
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            if (tooltip.activeInHierarchy)
+            {
+                HideDialog();
+            }
+        }
+    }
+
+    private void HideDialog()
+    {
+        tooltip.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = false;
+            HideDialog();
+        }
+    }
+}
