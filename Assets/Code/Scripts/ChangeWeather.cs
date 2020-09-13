2b42df52e536a60174dc83763fcf42636d8ff792
ploppowaffles <dcooper@usdd.com>
2020-09-12 17:31:51 -0700

    Added weather transitions

---
+++ Assets/Code/Scripts/ChangeWeather.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWeather : MonoBehaviour
+{
+    public bool toSunny;
+    public bool toCloudy;
+    public bool toRainStorm;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            EnvironmentManager.Instance.sunny = toSunny;
+            EnvironmentManager.Instance.cloudy = toCloudy;
+            EnvironmentManager.Instance.rainStorm = toRainStorm;
+        }
+    }
+}
