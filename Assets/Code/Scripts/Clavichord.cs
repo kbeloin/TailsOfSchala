ddf6625ef2b9ea9a9e0d958142811c235dea38aa
noidsoup <nicholas.eymann@gmail.com>
2020-09-26 15:50:35 -0700

    declaring the audio variable within the start function

--- Assets/Code/Scripts/Clavichord.cs
+++ Assets/Code/Scripts/Clavichord.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Clavichord : MonoBehaviour
-{
-
-    
+{    
+    private AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
         
     }
 
