ee01fa8489222098698c99fb1c442f7f9c6b14f8
melanie <melanie@cooperbold.com>
2020-10-31 23:14:21 -0700

    Create Quest object for Goat Chores

---
+++ Assets/Code/Scripts/Quests/GoatChores.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoatChores : Quest
+{
+    int wheat = 0;
+    int redApples = 0;
+    public bool hasWateredGoats;
+    public bool isComplete;
+
+
+
+    public override void Setup()
+    {
+        GameManager.Instance.itemAddDelegate += ItemAdded;
+        questName = "Feed the Goats";
+        description = "Mother wants you to collect 10 wheat and 5 red apples and deliver them to the goat feeder. Wheat is on the farm, but you'll have to go to the orchard in town for the apples.";
+        requirements = "10 Wheat and 5 Red Apples";
+    }
+
+    public override bool IsReadyToComplete()
+    {
+        if (wheat >= 10 && redApples >= 5 && hasWateredGoats)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public override void Complete()
+    {
+        if (!IsReadyToComplete()) return;
+
+        GameManager.Instance.RemoveInventoryItem("Wheat", 10);
+        GameManager.Instance.RemoveInventoryItem("Red Apple", 5);
+
+        isComplete = true;
+    }
+
+    void ItemAdded(string name)
+    {
+        Debug.Log("GoatChores saw item added: " + name);
+
+        if (name.Equals("Wheat")) wheat += 1;
+        if (name.Equals("Red Apple")) redApples += 1;
+
+        if (IsReadyToComplete()) Debug.Log("ThomasBirthdayQuest is ready to complete!");
+    }
+}
--- Assets/Resources/Dialogue/A1S1/A1S1_FeedGoats.ink
+++ Assets/Resources/Dialogue/A1S1/A1S1_FeedGoats.ink
@@ -6,9 +6,9 @@ Kay, now that you've eaten will you please see to your chores? #name:Mother
     What do I have to do this morning? #name:Kay
     Harvest some wheat, fill the goats' trough, collect a few apples, ... If you forget, check your journal. #name:Mother
     **[I'm on it.]
-        I'm on it! #name:Kay #mood:happy
+        I'm on it! #name:Kay #mood:happy #quest:GoatChores
     ->END
     **[But Mother, that's a lot to do.]
     I was hoping to read some more of my electronics textbook... #name:Kay #mood:sad
-    You can read to your heart's content after you do your chores. Hop to it! #name:Mother #mood:sad
+    You can read to your heart's content after you do your chores. Hop to it! #name:Mother #mood:sad #quest:GoatChores
     ->END
