c8060ebd33a48e4b79b1ba3660f4c9cfe2048ca6
ploppowaffles <dcooper@usdd.com>
2020-11-07 15:16:11 -0700

    more cleaning up of ThomasBirthdayBreakfast

--- Assets/Code/Scripts/Quest.cs
+++ Assets/Code/Scripts/Quest.cs
@@ -7,6 +7,7 @@ public abstract class Quest : IEquatable<Quest>
     public string questName;
     public string description;
     public string requirements;
+    public bool isComplete = false;
 
     public abstract void Setup();
     public abstract bool IsReadyToComplete();
